8d4641a7038b25cbfc1b84322f7228f14a1ca09c
rasindus <160561869+rasindus@users.noreply.github.com>
2025-03-27 16:05:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
-  OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
+  SESSION_ID: process.env.SESSION_ID || "6xIS0Qpb#PC_V8Znj4l0q0X966dsxVUejOVnkGwDIFlDxdQmSNeA",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:hQXDbDemiqTxqhsyOZImHnEfhdNVJVZk@turntable.proxy.rlwy.net:32949",
+  OWNER_NUM: process.env.OWNER_NUM || "94783364887",
 };
