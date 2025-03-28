cacae8641e15719d095e0981f45748a0627ff425
rasindus <160561869+rasindus@users.noreply.github.com>
2025-03-28 18:54:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,8 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "e4RGDQiI#7dKhCJ4UPX9zYjv5bQpdyUV23V4dd_bhMQRBAGsDxHw",
   OWNER_NUM: process.env.OWNER_NUM || "94783364887",
   PREFIX: process.env.PREFIX || ".",
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/rasindus/My-md/refs/heads/main/Gemini_Generated_Image_hxiqmihxiqmihxiq.jpg",
+  ALIVE_MSG: process.env.ALIVE_MSG || "හායි I am alive❤️😇 කරුනාකර විදානයක් ලබාදෙන්න🤍😊 𝚃𝚑𝚒𝚜 𝚋𝚘𝚝 𝚌𝚛𝚎𝚊𝚝𝚎𝚍 𝚋𝚢 𝚛𝚊𝚜𝚒𝚗𝚍𝚞🫂",
+  ‎AUTO_READ_STATUS: process.env.‎AUTO_READ_STATUS || "false",
+  MODE: process.env.MODE || "public",
 };
