ec77cdefc80d0934a32355b99bc0e6062f615304
rasindus <160561869+rasindus@users.noreply.github.com>
2025-03-28 18:31:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,5 @@ function convertToBool(text, fault = "true") {
 }
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "e4RGDQiI#7dKhCJ4UPX9zYjv5bQpdyUV23V4dd_bhMQRBAGsDxHw",
-  MONGODB: process.env.MONGODB || "mongodb://mongo:hQXDbDemiqTxqhsyOZImHnEfhdNVJVZk@turntable.proxy.rlwy.net:32949",
   OWNER_NUM: process.env.OWNER_NUM || "94783364887",
 };
