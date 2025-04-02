03c3ab6497b635fce3edf87027e1c9a81fdfbf2a
rasindus <160561869+rasindus@users.noreply.github.com>
2025-04-02 20:36:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -236,7 +236,7 @@ if (
     //Owner react
     if (senderNumber.includes("94783364887")) {
       if (isReact) return;
-      m.react("");
+      m.react("®️");
     }
     //work type
     if (!isOwner && config.MODE === "private") return;
